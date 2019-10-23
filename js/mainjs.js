bb62d168ef84d9436d438e86ee9fe55cea108f48
6heLL66 <51162582+6heLL66@users.noreply.github.com>
2019-10-23 19:50:49 +0400

    ыыва

--- js/mainjs.js
+++ js/mainjs.js
@@ -1,26 +1,3 @@
-$(document).ready(function (){
-		$(document).on('mousedown',dragstart);
-
-	});
-
-	function dragstart(e){
-		e.preventDefault();
-		mousepos = e.screenY;
-		$(document).on('mousemove',drag);
-		$(document).on('mouseup',dragstop);
-	}
-	function drag(e){
-		delta = mousepos - e.screenY;
-		to = setTimeout(function(){
-			$(document).scrollTop($(document).scrollTop()+delta);
-
-		},1);
-		mousepos = e.screenY;
-	}
-
-	function dragstop(e)	{
-		$(document).off('mousemove mouseup mouseout');
-	}
 
 
 
